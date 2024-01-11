08a03aee5f828143a277d53e1371234d94bed694
CasimiroGómez <casimiro.gomez@smartest.mx>
2024-01-11 13:13:05 -0600

    correcciones

--- tracy_be/settings.py
+++ tracy_be/settings.py
@@ -137,14 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/estatico/'
+STATIC_URL = 'estatico/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
 #Media
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
