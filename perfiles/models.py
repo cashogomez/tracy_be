3aeaa435df40535440f266a56d41e4ce8ee9fdd7
CasimiroGómez <casimiro.gomez@smartest.mx>
2024-06-06 10:17:25 -0600

    correciones de super usuario 7

--- perfiles/models.py
+++ perfiles/models.py
@@ -93,7 +93,7 @@ class Perfil(AbstractBaseUser):
     is_superadmin = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'nombre', 'paterno', 'materno', 'foto', 'telefono', 'empresa_id', 'numeroEmpleado']
+    REQUIRED_FIELDS = ['username', 'nombre', 'paterno', 'materno', 'foto', 'telefono', 'puesto', 'area', 'empresa_id', 'numeroEmpleado']
     
     objects = MiPerfilManager()
     
